69ef1c0a3e501b66ac8f2d59535aea21c19b440b
Umut Talha Çalışkan <umuttalhacaliskan@Umut-MacBook-Air.local>
2023-10-03 20:02:34 +0300

    timeout ve selenium geçiş önceğili ayarı

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pazarteam
+
+
+burada da boş dönerse script için de beautifulsoup la veya regexle bir şeyler yap 
+
+time.sleep yerine text yüklenene kadar bekle gibi bir şey ayarlanabilir
+
+link atıldığında level 1 değilse veritabanından level 2 için kotası var mı diye bak sonra o linke gitsin.
+
+timeout olursa error versin safyada
+
--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from flask import Flask,render_template,request,redirect,make_response,session,jsonify,url_for
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    make_response,
+    session,
+    jsonify,
+    url_for,
+    Response
+)
 from flask_mail import Mail, Message
 import os
 from dotenv import load_dotenv
@@ -9,48 +19,54 @@ import random
 from datetime import timedelta
 from urllib.parse import urlparse
 import time
-from datetime import datetime
+import signal
 
 from bs4 import BeautifulSoup
 from lxml import etree
 import requests
-import re 
+import re
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-chrome_driver_path = './chromedriver'
+chrome_driver_path = "./chromedriver"
 options = Options()
-options.add_argument('--headless')  
-
-
-
+options.add_argument("--headless")
+options.add_argument(
+    "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+)
+options.add_argument("--enable-javascript")
 
 load_dotenv()
 
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder="static", static_url_path="")
 app.secret_key = secrets.token_urlsafe(16)
 app.permanent_session_lifetime = timedelta(minutes=30)
 
 mail_settings = {
-    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_SERVER": "smtp.gmail.com",
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.environ['EMAIL_USER'],
-    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD']
+    "MAIL_USERNAME": os.environ["EMAIL_USER"],
+    "MAIL_PASSWORD": os.environ["EMAIL_PASSWORD"],
 }
 
 
 app.config.update(mail_settings)
 mail = Mail(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
+
 class TierEnum(Enum):
-    FREE = 'Free'
-    PREMIUM = 'Premium'
+    FREE = "Free"
+    PREMIUM = "Premium"
+
 
 class User(db.Model):
     email = db.Column(db.String, primary_key=True, unique=True, nullable=False)
@@ -64,83 +80,90 @@ class Product(db.Model):
 
 
 
-def change_price_mail(receiver_mail,website_link):
 
-    msg = Message(subject="Price Changed", sender=app.config.get("MAIL_USERNAME"), recipients=[receiver_mail], body=f"""
+def change_price_mail(receiver_mail, website_link):
+    msg = Message(
+        subject="Price Changed",
+        sender=app.config.get("MAIL_USERNAME"),
+        recipients=[receiver_mail],
+        body=f"""
     Hi!
                     
     The price of the product you are following has changed. Here is the product link {website_link}
-    """)
+    """,
+    )
     mail.send(msg)
+
 
 def generate_client_id():
     return str(random.randint(1, 999999))
 
 
+def verileri_al(url, price):
+    try:
+        response = requests.get(url, timeout=5)
+        print(response.text)
+    except requests.Timeout:
+        return {"elementler": elements_check, "scrap_type": "request"}
+    except requests.RequestException as e:
+        return {"elementler": elements_check, "scrap_type": "request"}
 
-def verileri_al(url,price):
+    
+    
+    soup = BeautifulSoup(response.content, "html.parser")
 
-    response = requests.get(url)
-    
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
+
     elements = soup.find_all(string=re.compile(price))
+    elements_check = []
 
-    if elements ==[]:
+    for element in elements:
+        parent = element.find_parent()
 
+        if parent and parent.name == "script":
+            continue
+
+        if parent and parent.name == "style":
+            continue
+
+        elements_check.append(element)
+
+        # print(f'Tag: {parent if parent else None}, Sınıf: {class_list}, İçerik: {element}')
+
+    elements_check_selenium = []
+
+    if elements_check == []:
         driver = webdriver.Chrome(executable_path=chrome_driver_path, options=options)
 
         driver.get(url)
+
+        time.sleep(1)
+
+        # wait = WebDriverWait(driver, 10)
+
+        # price_element = wait.until(EC.text_to_be_present_in_element((By.TAG_NAME, "body"), price))
 
         page_content = driver.page_source
 
         driver.quit()
 
-        soup = BeautifulSoup(page_content, 'html.parser')
-    
+        soup = BeautifulSoup(page_content, "html.parser")
+
         elements1 = soup.find_all(string=re.compile(price))
 
         for element in elements1:
             parent = element.find_parent()
-            
-            if parent and parent.name == 'script' or "style":
+
+            if parent and parent.name == "script":
                 continue
-            
-            if parent and 'class' in parent.attrs:
-                class_list = parent['class']
-            else:
-                class_list = None
 
-            print(f'Tag: {parent.name if parent else None}, Sınıf: {class_list}, İçerik: {element}')
+            if parent and parent.name == "style":
+                continue
 
-            return {
-                "url": url,
-            }
+            elements_check_selenium.append(element)
 
-    
+        return {"elementler": elements_check_selenium, "scrap_type": "level 2"}
 
-    for element in elements:
-        parent = element.find_parent()
-
-        
-        if parent and parent.name == 'script' or "style":
-            continue
-        
-        if parent and 'class' in parent.attrs:
-            class_list = parent['class']
-        else:
-            class_list = None
-
-        print(f'Tag: {parent.name if parent else None}, Sınıf: {class_list}, İçerik: {element}')
-
-
-    return {
-        "url": url,
-        # "urun_title":urun_title,
-        # "urun_fiyat": urun_fiyat,
-    }
-
-
+    return {"elementler": elements_check, "scrap_type": "level 1"}
 
 
 @app.route("/")
@@ -148,55 +171,67 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/process", methods=['POST'])
+@app.route("/process", methods=["POST"])
 def process():
-    input_data = request.form.get('inputData')
+    input_data = request.form.get("inputData")
 
     # Kullanıcıya özgü bir oturum kimliği oluşturun
     session_token = secrets.token_urlsafe(16)
 
     # Kullanıcı oturum bilgilerini saklayın (örneğin, ürün linki ve input data)
-    session[session_token] = {'input_data': input_data}
+    session[session_token] = {"input_data": input_data}
 
-    return redirect(f'/products/{session_token}')
+    return redirect(f"/products/{session_token}")
 
 
-@app.route('/products/<session_token>')
+@app.route("/products/<session_token>")
 def products(session_token):
     # Kullanıcının oturumunu kontrol edin
     user_data = session.get(session_token)
 
-    print(user_data)
+    # print(user_data)
 
     if not user_data:
-        return redirect('/')
+        return redirect("/")
 
     # Veritabanına kaydetmek istediğiniz bilgileri alın
-    input_data = user_data.get('input_data')
+    input_data = user_data.get("input_data")
 
     # Burada veritabanına kaydetme işlemlerini yapabilirsiniz (örneğin SQLAlchemy kullanabilirsiniz)
 
-    return render_template('products.html', input_data=input_data)
+    return render_template("products.html", input_data=input_data)
 
 
-@app.route("/deneme", methods=['POST'])
+@app.route("/deneme", methods=["POST"])
 def deneme():
-    title = request.form.get('title')
-    url = request.form.get('url')
-    price = request.form.get('price')
+    title = request.form.get("title")
+    url = request.form.get("url")
+    price = request.form.get("price")
 
-    verileri_al(url,price)
-    
-    client_id = "id"+ generate_client_id()
+    elemet_list = verileri_al(url, price)
 
-    return render_template('accordion.html', title=title, url=url,price=price,client_id=client_id)
+    print(elemet_list["scrap_type"])
+    print(elemet_list["elementler"])
+
+    client_id = "id" + generate_client_id()
+
+    return render_template(
+        "accordion.html",
+        title=title,
+        url=url,
+        price=price,
+        client_id=client_id,
+        scrap_type=elemet_list["scrap_type"],
+        elementler=elemet_list["elementler"][0],
+    )
+
 
 #     # print(request.__dict__)
-    
+
 #     # user_ip = request.remote_addr
 
 
 if __name__ == "__main__":
     app.app_context().push()
     db.create_all()
-    app.run(debug=True) 
+    app.run(debug=True)
